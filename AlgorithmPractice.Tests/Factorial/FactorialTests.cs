f20135b2f82efa74c5c975ada4b0fb17b719233c
Matt <matt@imaginecareers.com>
2017-03-14 08:06:15 -0400

    added the factorial problem - both iterative and recursive

---
+++ AlgorithmPractice.Tests/Factorial/FactorialTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace AlgorithmPractice.Tests
+{
+    [TestClass]
+    public class FactorialTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Iterative_negative_value_should_throw_exception()
+        {
+            Factorial.Iterative(-1);
+        }
+
+        [TestMethod]
+        public void Iterative_should_return_correct_value_for_0()
+        {
+            Factorial.Iterative(0).Should().Be(1);
+        }
+
+        [TestMethod]
+        public void Iterative_should_return_correct_value()
+        {
+            Factorial.Iterative(1).Should().Be(1);
+            Factorial.Iterative(5).Should().Be(120);
+            Factorial.Iterative(25).Should().Be(7034535277573963776);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Recursive_negative_value_should_throw_exception()
+        {
+            Factorial.Recursive(-1);
+        }
+
+        [TestMethod]
+        public void Recursive_should_return_correct_value_for_0()
+        {
+            Factorial.Recursive(0).Should().Be(1);
+        }
+
+        [TestMethod]
+        public void Recursive_should_return_correct_value()
+        {
+            Factorial.Recursive(1).Should().Be(1);
+            Factorial.Recursive(5).Should().Be(120);
+            Factorial.Recursive(15).Should().Be(1307674368000);
+            Factorial.Recursive(25).Should().Be(7034535277573963776);
+        }
+    }
+}
---
+++ AlgorithmPractice/Factorial/Factorial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice
+{
+    public static class Factorial
+    {
+        public static long Iterative(int n)
+        {
+            CheckAssertions(n);
+
+            long result = 1;
+            for (int i = n; i > 0; i--)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        public static long Recursive(long n)
+        {
+            CheckAssertions(n);
+
+            if (n == 0)
+                return 1;
+
+            return n * Recursive(n - 1);
+        }
+
+        private static void CheckAssertions(long n)
+        {
+            if (n < 0)
+                throw new Exception($"Value ({n}) must be positive.");
+        }
+    }
+}
